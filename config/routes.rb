d476f7e91c9a04285d483b69d77f206b57567f1e
M Santosh Dora <santosh.dora@chronus.com>
2019-06-28 14:09:32 +0530

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
 
   get  '/help', to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
@@ -8,4 +9,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  resources :users
+
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  #Verify that clicking the signup button
+  #results in not creating a new user when the submitted information is invalid.
+  test "invalid form submission" do
+  	get signup_path
+  	assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name:  "",
+                                     email: "user@invalid",
+                                     password:              "foo",
+                                     password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'form[action="/signup"]'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert.alert-danger'
+    assert_select "li", "Name can't be blank"
+    assert_select "li", "Email is invalid"
+    assert_select "li", "Password confirmation doesn't match Password"
+
+
+   # assert_select 'div.<CSS class for field with error>'
+
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+
+end
